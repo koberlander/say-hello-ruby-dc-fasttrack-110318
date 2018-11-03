8d47601692171c5a21df8438bf27abd1eee9f28b
Kristin M Oberlander <koberlander115@gmail.com>
2018-11-03 18:05:03 +0000

    automatically backed up by learn

---
+++ array-method.rb
@@ -0,0 +1,10 @@
+cats = ["Garfield", "Nala", "Meowth"]
+
+cats.push("Felix")
+# cats.push("Fluffy")
+
+#cats.unshift("Fluffy")
+abandonedCats = cats.shift 
+p cats
+puts "------"
+puts abandonedCats
